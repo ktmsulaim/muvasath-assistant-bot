5b31dd7b4be6b40afaf71b0e9e6b74d07a1c7ca0
K.T MOHAMMED SULAIM <ktmsulaim@gmail.com>
2022-04-04 23:05:55 +0530

    Inital commit, basic batch wise members total api

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const { Telegraf } = require("telegraf");
+const axios = require("axios");
+
+const api = new axios.create({
+  baseURL: process.env.BASE_URL,
+});
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.hears(["Hi", "hi"], (ctx) => ctx.reply("Hey there!"));
+
+bot.command("batch_wise_members_total_amount", async (ctx) => {
+  try {
+    const response = await api.get("/campaigns/stats/batchWise");
+
+    ctx.reply(response.data);
+  } catch (error) {
+    ctx.reply("Sorry! some thing went wrong!");
+  }
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
